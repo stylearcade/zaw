59b5dce60bca24fd1e719c574531d4a57aac7589
Tristan Hoy <tristan.hoy@gmail.com>
2025-11-05 23:44:33 +1100

    Upgrade to zig 0.15

--- implementations/wasm-zig/src/interop.zig
+++ implementations/wasm-zig/src/interop.zig
@@ -14,15 +14,15 @@ pub const logf = logModule.logf;
 var input: conduit.Reader = undefined;
 var output: conduit.Writer = undefined;
 
-pub fn getErrorPtr() callconv(.C) i32 {
+pub fn getErrorPtr() callconv(.c) i32 {
     return Error.getErrorPtr();
 }
 
-pub fn getLogPtr() callconv(.C) i32 {
+pub fn getLogPtr() callconv(.c) i32 {
     return logModule.getLogPtr();
 }
 
-pub fn allocateInputChannel(sizeInBytes: i32) callconv(.C) i32 {
+pub fn allocateInputChannel(sizeInBytes: i32) callconv(.c) i32 {
     const sizeInU64s = @divExact(@as(usize, @intCast(sizeInBytes)), 8);
     const storage = std.heap.wasm_allocator.alloc(u64, sizeInU64s) catch @panic("Failed to allocate input channel storage");
     const pointer: i32 = @intCast(@intFromPtr(storage.ptr));
@@ -32,7 +32,7 @@ pub fn allocateInputChannel(sizeInBytes: i32) callconv(.C) i32 {
     return pointer;
 }
 
-pub fn allocateOutputChannel(sizeInBytes: i32) callconv(.C) i32 {
+pub fn allocateOutputChannel(sizeInBytes: i32) callconv(.c) i32 {
     const sizeInU64s = @divExact(@as(usize, @intCast(sizeInBytes)), 8);
     const storage = std.heap.wasm_allocator.alloc(u64, sizeInU64s) catch @panic("Failed to allocate output channel storage");
     const pointer: i32 = @intCast(@intFromPtr(storage.ptr));
