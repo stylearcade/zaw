20269493f5d9b3ad672a7cf9ba996741cd3f1e1f
Tristan Hoy <tristan.hoy@gmail.com>
2025-11-05 23:44:33 +1100

    Upgrade to zig 0.15

--- implementations/wasm-zig/build.zig
+++ implementations/wasm-zig/build.zig
@@ -9,4 +9,15 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
+
+    const test_step = b.step("test", "Run unit tests");
+
+    const unit_tests = b.addTest(.{ .root_module = b.createModule(.{
+        .root_source_file = b.path("src/conduit/conduit.test.zig"),
+        .target = target,
+    }) });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    test_step.dependOn(&run_unit_tests.step);
 }
--- implementations/wasm-zig/src/interop/externs.zig
+++ implementations/wasm-zig/src/interop/externs.zig
@@ -1,7 +1,10 @@
 const builtin = @import("builtin");
 
-pub usingnamespace if (builtin.target.cpu.arch == .wasm32) struct {
+// Allow no-op non-externs in native mode to enable IDE test runs
+const impl = if (builtin.target.cpu.arch == .wasm32) struct {
     pub extern fn hostLog() void;
 } else struct {
     pub fn hostLog() void {}
 };
+
+pub const hostLog = impl.hostLog;
