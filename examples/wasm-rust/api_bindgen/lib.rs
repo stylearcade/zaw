558adc91a867fa17d86caae9c563aa02904c133e
Taj Pereira <taj@canva.com>
2026-01-24 06:48:05 +1030

    Use simd + pass slices

--- examples/wasm-rust/api_bindgen/lib.rs
+++ examples/wasm-rust/api_bindgen/lib.rs
@@ -1,32 +1,18 @@
 use wasm_bindgen::prelude::*;
-use js_sys::Int32Array;
-use js_sys::Float32Array;
-use js_sys::Float64Array;
 
 #[wasm_bindgen]
-pub fn xor_array_i32(values: &Int32Array) -> i32 {
-    let vec = values.to_vec();
-
-    return shared::xor_array_i32(&vec);
+pub fn xor_array_i32(values: &[i32]) -> i32 {
+    shared::xor_array_i32(values)
 }
 
 #[wasm_bindgen]
-pub fn sum_array_f64(values: &Float64Array) -> f64 {
-    let vec = values.to_vec();
-
-    return shared::sum_array_f64(&vec);
+pub fn sum_array_f64(values: &[f64]) -> f64 {
+    shared::sum_array_f64(values)
 }
 
 #[wasm_bindgen]
-pub fn multiply_4x4_f32(
-    a_matrices: &Float32Array,
-    b_matrices: &Float32Array
-) -> Result<Float32Array, JsValue> {
-    let a_data: Vec<f32> = a_matrices.to_vec();
-    let b_data: Vec<f32> = b_matrices.to_vec();
-    let mut results = vec![0.0f32; a_data.len()];
-
-    shared::multiply_4x4_f32(&a_data, &b_data, &mut results);
-
-    Ok(Float32Array::from(&results[..]))
+pub fn multiply_4x4_f32(a_matrices: &[f32], b_matrices: &[f32]) -> Vec<f32> {
+    let mut results = vec![0.0f32; a_matrices.len()];
+    shared::multiply_4x4_f32(a_matrices, b_matrices, &mut results);
+    results
 }
