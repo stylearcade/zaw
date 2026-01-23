7d6ae5aa7298832ed401a1d1a0ba3678226a44bd
Taj Pereira <taj@canva.com>
2026-01-24 06:48:05 +1030

    Use simd + pass slices

--- scripts/build-rust.sh
+++ scripts/build-rust.sh
@@ -4,7 +4,7 @@ set -e;
 ROOT="$(dirname $(realpath $0))/..";
 
 cd $ROOT/examples/wasm-rust/api_bindgen
-wasm-pack build --target web
+RUSTFLAGS="-C target-feature=+simd128" wasm-pack build --target web
 wasm2wat ./pkg/wasm_api_bindgen_bg.wasm >./pkg/wasm_api_bindgen_bg.wat || true
 
 cd $ROOT/examples/wasm-rust/api_zaw
